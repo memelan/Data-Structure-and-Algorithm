6bb0f8e9e98be1014ee921b6868fee9ddfb02b0e
elainemeilan <memelan@users.noreply.github.com>
2020-04-17 14:34:43 +0800

    Create 1 DS_in_Python_Exercise.py

---
+++ 1 DS_in_Python_Exercise.py
@@ -0,0 +1,28 @@
+# Chapter 1. Python Primer - Exercises (Reinforcement 1-12, Creativity 13-28, Projects 29-36)
+
+''' 1 Write a short Python function, is multiple(n, m), that takes two integer
+values and returns True if n is a multiple of m, that is, n = mi for some
+integer i, and False otherwise.'''
+
+# solution: 
+
+Def mutiple(n,m):           # m,n is integer
+  if m % n == 0: 
+      Return True
+  else:
+      Return False
+
+
+''' 2 Write a short Python function, is even(k), that takes an integer value and
+returns True if k is even, and False otherwise. However, your function
+cannot use the multiplication, modulo, or division operators. '''
+
+# solution:
+
+
+'''3 Write a short Python function, minmax(data), that takes a sequence of
+one or more numbers, and returns the smallest and largest numbers, in the
+form of a tuple of length two. Do not use the built-in functions min or
+max in implementing your solution.'''
+
+# solution:
